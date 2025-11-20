040a733ab69fd55afebeefc5f440db69eb4f52b9
webnex12 <ahmadmouallem210@gmail.com>
2025-11-20 10:49:53 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public')); // serve files from public/
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
